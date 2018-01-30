403bc07f53be84e08d0481a7bc915be39ea82a0e
subinpvasu <subinpvasu@gmail.com>
2018-01-31 00:48:23 +0530

    Mysql Test

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
 
-echo phpinfo();
+$url = parse_url(getenv("mysql://b6cdd705707835:8cf82617@us-cdbr-iron-east-05.cleardb.net/heroku_6bdbd53fbe793dd?reconnect=true"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully -".$db;
+
+/*$result = $conn->query("SELECT NOW()");
+$row = mysqli_fetch_array($result);*/
+echo '<pre>';
+print_r($url);
+echo '</pre>';
+
+//echo phpinfo();
