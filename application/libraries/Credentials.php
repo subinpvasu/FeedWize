b860eacabcd82c370e1e47e893184e4328edea25
subinpvasu <subinpvasu@gmail.com>
2018-02-03 00:50:14 +0530

    Codeigniter 3 used for developement, which will ensure teh security related issues and will give a solid start.

---
+++ application/libraries/Credentials.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ *  Mail : subinpvasu@gmail.com
+ *  Skype : subinpvasu 
+ *  AdWords API integration
+ */
+ 
+class Credentials{
+   public static $MASTER_ID = '442-610-5947';
+    public static $ACCOUNT_ID = '1432268762';
+    public static $CLIENT_ID = '696341579407-1p8uth7mlhue6kt1v380bltoqheh4abs.apps.googleusercontent.com';
+    public static $CLIENT_SECRET = 'QVCxuXMSclGFm4fa6_ry_BEc';
+    public static $DEVELOPER_TOKEN = '-vdri0xcKwiTQNPkD4GDAg';
+    public static $REFRESH_TOKEN = '1/iHS-IoK_dJaqV-dXfXDPJz9jogp61vqkXIqmY-67xLZugwZ3Fmfyq2c1wyQ3nZZK';
+    public static $STATUS_FIELD = 16;
+    public static $PAGE_LIMIT = 500;
+    
+ 
+            
+}
---
+++ application/libraries/Processor.php
@@ -0,0 +1,38 @@
+<?php
+ob_start();
+/*
+ *  Mail : subinpvasu@gmail.com
+ *  Skype : subinpvasu 
+ *  AdWords API integration
+ */
+require_once './Credentials.php';
+require_once './Advertising.php';
+
+use Adwords\Advertising;
+class Processor {
+    protected $advertising;
+    protected $debug = false;
+    
+    public function __construct()
+    {
+        
+        
+        $arr = [
+            'OAUTH2' => [
+                'developerToken' => Credentials::$DEVELOPER_TOKEN,
+                'clientId' => Credentials::$CLIENT_ID,
+                'clientSecret' => Credentials::$CLIENT_SECRET,
+                'refreshToken' => Credentials::$REFRESH_TOKEN,
+            ]
+        ];
+
+        $this->advertising = new Advertising($arr,  Credentials::$MASTER_ID);
+    }
+ 
+    public function modify_account($account,$status,$leave_some)
+    {
+            $session = $this->advertising->createSession($account);
+            return $this->advertising->ModifyAccount($this->advertising->getAdwordsServices(),$session,$status,$leave_some); 
+    }
+}
+
