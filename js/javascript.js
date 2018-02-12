6afdad61a65d627d35dbb11349d15dd9d48dd382
subinpvasu <subinpvasu@gmail.com>
2018-02-12 23:05:06 +0530

    login function and user insertion trial 12

--- js/javascript.js
+++ js/javascript.js
@@ -38,7 +38,8 @@ function onSignIn(googleUser) {
 		 },
 		success:function(result){
                     if($("#page").val()=='login'){
-//                    window.location.reload(true);}
+//                    window.location.reload(true);
+                        }
                 }
             });
   
