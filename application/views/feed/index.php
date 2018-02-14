94e2160e38d9365ebf5f508e43a890723fdb9f70
subinpvasu <subinpvasu@gmail.com>
2018-02-15 01:03:48 +0530

    project started trial 2

--- application/views/feed/index.php
+++ application/views/feed/index.php
@@ -8,7 +8,7 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-lg-12">
-                        
+                        <h2><?php echo $welcome ?></h2>
                     
                     </div>
                 </div>
